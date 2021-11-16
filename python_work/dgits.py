79a1364f8ed976519b5ea0b77641aafd4b0f26f1
OldFreddy <oldfredddy@gmail.com>
2021-11-16 15:56:43 +0300

    digits

---
+++ python_work/dgits.py
@@ -0,0 +1,30 @@
+a = 3 / 2
+print(a)
+
+a = 3 ** 3
+print(a)
+
+a = 2 + 3 * 4
+print(a)
+
+a = (2 + 3) * 4
+print(a)
+
+a = 0.1 + 0.1
+print(a)
+
+a = 0.2 + 0.1
+print(a)
+
+a = 4 / 2
+print(a)
+
+universe_age = 14_000_000_000
+print(universe_age)
+
+x, y, z = 0, 2, 5
+print(x, y, z)
+
+fav = 5
+a = f"Мое любимое число {fav}"
+print(a)
