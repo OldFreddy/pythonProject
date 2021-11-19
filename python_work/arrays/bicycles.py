a1d1d90df6212320bb2de328e2372a3ac9599e16
OldFreddy <oldfredddy@gmail.com>
2021-11-19 12:50:10 +0300

    arrays

---
+++ python_work/arrays/bicycles.py
@@ -0,0 +1,7 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[-1]) #выводит последний элемент списка
+message = f"My first bicycle was a {bicycles[0].upper()}"
+print(message)
---
+++ python_work/arrays/motorcycles.py
@@ -0,0 +1,15 @@
+motorcucles = ['honda', 'yamaha', 'suzuki']
+print(motorcucles)
+
+motorcucles[0] = 'ducati'
+print(motorcucles)
+
+
+motorcucles.append('ducati') #добавление элемента в конец списка
+print(motorcucles)
+
+motorcucles = []
+motorcucles.append('hondaa')
+motorcucles.append('yamahaa')
+motorcucles.append('suzukii')
+print(motorcucles)
