c812ff5fc00622b1a6e5866d197a9bb8068afd00
oldfreddy <oldfredddy@gmail.com>
2022-02-07 14:22:00 +0300

    07.02.2022

--- python_work/if/if со списками/Hello Admin.py
+++ python_work/if/if со списками/Hello Admin.py
@@ -1,5 +1,5 @@
 users = ['Admin', 'user1', 'user2', 'user3', 'user4']
-#user = []
+# user = []
 if users:
     for user in users:
         if user == 'Admin':
@@ -8,3 +8,14 @@ if users:
             print(f'Hello {user}')
 else:
     print("Скисок пользователей пуст")
+
+current_users = ['User1', 'user2', 'user3', 'user4', 'user5']
+new_users = ['user1', 'user2', 'user6', 'user7', 'user8']
+current_users_lower_case = []
+for user in current_users:
+    current_users_lower_case.append(user.lower())
+for user in new_users:
+    if user in current_users_lower_case:
+        print(f'пользователь с именем {user} уже есть, выберите новое имя')
+    else:
+        print(f'имя {user} свободно, можешь юзать его')
