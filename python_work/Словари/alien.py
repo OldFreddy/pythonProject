8ee1b09dfb317f930c703b1381dea13a22963d81
oldfreddy <oldfredddy@gmail.com>
2022-02-28 16:59:26 +0300

    28.02.2022

--- python_work/Словари/alien.py
+++ python_work/Словари/alien.py
@@ -9,3 +9,45 @@ print(f"You just earned {new_points} points!")
 alien_0['x_position'] = 0
 alien_0['y_position'] = 25
 print(alien_0)
+
+#Создание пустого словаря
+
+alien_0 = {}
+
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+
+print(alien_0)
+
+#Изменение значений в словаре
+
+alien_0 = {'color': 'green'}
+print(f"The alien is {alien_0['color']}.")
+
+alien_0['color'] = 'yellow'
+print(f'The alien is now {alien_0["color"]}')
+print('/n')
+
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print(f"Original position: {alien_0['x_position']}")
+# Пришелец перемещается вправо
+# ычисляем величину смещения на основании текущей скорости
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    #Пришелец двигается быстро
+    x_increment = 3
+# Новая позиция равна сумме старой позиции и приращения.
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print(f"New position: {alien_0['x_position']}")
+
+#Удаление пар "Ключ-значение"
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0['points'])
+
+del alien_0['points']
+print(alien_0)
