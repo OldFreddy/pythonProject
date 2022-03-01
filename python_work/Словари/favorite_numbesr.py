558673831525fe4d903970f222dfdc7cc243a1c1
oldfreddy <oldfredddy@gmail.com>
2022-03-01 14:27:13 +0300

    01.03.2022 Словари

---
+++ python_work/Словари/favorite_numbesr.py
@@ -0,0 +1,2 @@
+favorite_numbers = {'Nik': 1, 'Vova': 2}
+print(f'Никита любит число {favorite_numbers.get("Nik")}')
---
+++ python_work/Словари/person.py
@@ -0,0 +1,4 @@
+person_0 = {'name': 'Nikitos', 'age': 23}
+print(person_0.get('name'))
+print(person_0['name'].upper())
+print(person_0.get('status'))
