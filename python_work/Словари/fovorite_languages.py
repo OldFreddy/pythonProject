5c3c0476a0b8b0e1f931c30e7aab63ea29b3fbe6
oldfreddy <oldfredddy@gmail.com>
2022-03-02 12:48:17 +0300

    02.03.2022 Словари

--- python_work/Словари/fovorite_languages.py
+++ python_work/Словари/fovorite_languages.py
@@ -5,6 +5,20 @@ favorite_languages = {
     'phil': 'python'
     }
 language = favorite_languages['sarah'].title()
+language = favorite_languages['edward'].title()
 print(language)
 
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}")
 
+for name in favorite_languages.keys():
+    print(name.title())
+
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
