527154dc4afe7097290630465f14ef3fe2715f1e
freddy <oldfredddy@gmail.com>
2022-04-18 16:44:03 +0300

    18.04.2022 -

---
+++ python_work/classes/dog.py
@@ -0,0 +1,18 @@
+class Dog():
+    """Простая модель собаки"""
+
+    def __init__(self, name, age):
+        """Инициализирует атрибуты name и age"""
+
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Собака садится по команде"""
+        print(f"{self.name} is now sitting ")
+
+    def roll_over(self):
+        """Собака перекатывается по команде"""
+        print(f"{self.name} rolled over")
+
+
--- python_work/name_cases.py
+++ python_work/name_cases.py
@@ -2,3 +2,7 @@ name = "sasha grey"
 print(name.lower())
 print(name.upper())
 print(name.title())
+
+
+print(name)
+
