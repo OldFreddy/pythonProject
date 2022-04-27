090cbee8eb6e7de517a969718028d29ede1be901
freddy <oldfredddy@gmail.com>
2022-04-27 14:06:34 +0300

    27.04.2022 - 183 page

--- python_work/classes/Car.py
+++ python_work/classes/Car.py
@@ -29,19 +29,36 @@ class Car():
         self.odometer_reading += miles
 
 
+# my_new_car = Car('audi', 'a4', 2019)
+# print(my_new_car.get_descriptive_name())
+# my_new_car.odometer_reading = 23
+# my_new_car.update_odometer(500)
+# my_new_car.update_odometer(300)
+# my_new_car.read_odometer()
+#
+# my_used_car = Car('subaru', 'outback', 2015)
+# print(my_new_car.get_descriptive_name())
+#
+# my_used_car.update_odometer(23_500)
+# my_used_car.read_odometer()
+#
+# my_used_car.increment_odometer(200)
+# my_used_car.read_odometer()
 
-my_new_car = Car('audi', 'a4', 2019)
-print(my_new_car.get_descriptive_name())
-my_new_car.odometer_reading = 23
-my_new_car.update_odometer(500)
-my_new_car.update_odometer(300)
-my_new_car.read_odometer()
+class Electro_Car(Car):
+    """Представляют аспекты машины, специфические для электромобилей"""
 
-my_used_car = Car('subaru', 'outback', 2015)
-print(my_new_car.get_descriptive_name())
+    def __init__(self, make, model, year):
+        """инициализирует атрибуты класса-родителя.
+        Затем инициализирует атрибуты, специфические для электромобиля"""
+        super().__init__(make, model, year)
+        self.battery_size = 75
 
-my_used_car.update_odometer(23_500)
-my_used_car.read_odometer()
+    def describe_battery(self):
+        """Выводит информацию о мощности аккумуляторов"""
+        print(f"This car has a {self.battery_size} kWh battery")
 
-my_used_car.increment_odometer(200)
-my_used_car.read_odometer()
+
+my_tesla = Electro_Car('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
