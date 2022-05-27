13ab70d6df49e42f3498c376a3e2dc9262ac6a2f
OldFredddy <Freddyforever1989>
2022-05-27 13:30:28 +0300

    27.04.2022 - 193 page

--- python_work/classes/car.py
+++ python_work/classes/car.py
@@ -38,37 +38,4 @@ class Car():
             self.odometer_reading += miles
 
 
-class Battery():
-    """Простая модель аккумулятора автомобиля"""
 
-    def __init__(self, battery_size=75):
-        """Инициализация атрибутов аккумулятора"""
-        self.battery_size = battery_size
-
-    def describe_battery(self):
-        """Выводит информацию о мощности аккумулятора"""
-        print(f"{self.battery_size}")
-
-
-
-    def get_range(self):
-        """Выводит приблизительный запас хода для аккумулятора"""
-        if self.battery_size == 75:
-            range = 260
-        elif self.battery_size == 100:
-            range = 315
-
-        print(f"This car can go about {range} miles on a full charge.")
-
-
-class ElectricCar(Car):
-    """Представляет аспекты машины, специфические для электромобилей."""
-
-    def __init__(self, make, model, year):
-        """
-        Инициализирует атрибуты класса-родителя.
-        Затем инициализирует атрибуты, специфические для электромобиля.
-        """
-
-        super().__init__(make, model, year)
-        self.battery = Battery()
